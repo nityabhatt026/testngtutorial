e3049bb4543b53da851cea323347862a01fe5513
nityabhatt026 <nitya.bhatt@appdirect.com>
2017-03-23 00:58:19 +0530

    Update TestClass.java

--- TestMaven/src/test/java/com/appdirect/nitya/TestMaven/TestClass.java
+++ TestMaven/src/test/java/com/appdirect/nitya/TestMaven/TestClass.java
@@ -1,29 +1,48 @@
 package com.appdirect.nitya.TestMaven;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.mustache.Value;
+
+import com.appdirect.nitya.utils.Arguments;
 
 public class TestClass {
+	
 	ChromeDriver driver;
+    String url;
+    String searchName;
     
 	@Test
-	@Parameters({"url","search_name","browser_path","browser_driver"})
-	public void openBrowser(String url,String search_name,String browser_path,String browser_driver) throws InterruptedException{
-	Reporter.log("############");
-	System.setProperty(browser_path,browser_driver);
-	Reporter.log("Opening Chrome Browser");
-    driver= new	ChromeDriver();
-    Reporter.log("Opening Url");
-    driver.get(url);
-    Reporter.log("Selecting search bar");
-    WebElement element = driver.findElement(By.id("lst-ib"));
-    Reporter.log("Making Search");
-    element.sendKeys(search_name);
-    driver.findElement(By.id("_fZl")).click();
-    
+	public void openBrowser() throws Exception{
+		Properties prop = new Properties();
+		try
+		{
+		InputStream input = new FileInputStream(new File("resources/Value.properties"));
+		prop.load(input);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+        url=prop.getProperty("url");
+		searchName=prop.getProperty("searchName");	
+		Arguments obj=new Arguments();
+		Reporter.log("Opening Chrome Browser");
+		WebDriver driver= obj.setPath();
+		Reporter.log("Opening Url");
+	    driver.get(url);
+	    WebElement element1 =obj.finder("lst-ib");
+	    Reporter.log("Selecting search bar");
+	    element1.sendKeys(searchName);
+	    Reporter.log("Making Search");
+	    WebElement element2=obj.finder("_fZl");
+	    element2.click();   
 	}
 }
